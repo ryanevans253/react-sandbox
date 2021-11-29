9c1bef04f492e06b4626732c506c27804a4e345c
Ryan <ryanevans253@yahoo.com>
2021-11-29 14:12:39 -0500

    plant redo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import { Avatar } from "@mui/material";
 import React, { useState } from "react";
-import AddButton from "./components/AddButton";
-import Todo from "./components/Todo";
-import ChangeColor from "./components/color";
+import PermanentDrawerLeft from "./components/SidebarNav";
 
 function App() {
   return (
     <div>
-      <div>
-        <Avatar />
-        <h1>Ryan's to do list</h1>
-      </div>
-      <Todo name="ryanne" />
-      <ChangeColor />
+      <PermanentDrawerLeft />
     </div>
   );
 }
